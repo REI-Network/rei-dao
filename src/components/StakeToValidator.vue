26d0f170549ee34ad1ac3d26238e0fbaa08969b6
xxx <huanghanxue@gxb.io>
2022-06-22 11:18:08 +0800

    chore: update my voted validator apy

--- src/components/StakeToValidator.vue
+++ src/components/StakeToValidator.vue
@@ -171,9 +171,9 @@
                   <template v-slot:item.balannceOfShare="{ item }">
                     {{ item.balannceOfShare | asset(2) }}
                   </template>
-                  <template v-slot:item.apy="{ item }">
+                  <!-- <template v-slot:item.apy="{ item }">
                     {{ item.apy | asset(2) }}
-                  </template>
+                  </template> -->
 
                   <template v-slot:item.actions="{ item }">
                     <v-btn tile small color="vote_button" class="mr-4 btn-radius font-btn" style="color: #fff" @click.stop="handleStaking(item)" height="32">
@@ -642,7 +642,7 @@ export default {
           value: 'power'
         },
         { text: this.$t('stake.share_balance'), value: 'balannceOfShare' },
-        { text: 'Apy', value: 'apy' },
+        // { text: 'Apy', value: 'apy' },
         { text: this.$t('stake.operation'), value: 'actions', sortable: false }
       ],
       status: {
