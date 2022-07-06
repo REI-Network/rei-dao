1716713e9021ea490275034f97ba96d016284fd6
xxx <huanghanxue@gxb.io>
2022-07-06 17:59:17 +0800

    chore: update bridge content

--- src/components/BridgeUser.vue
+++ src/components/BridgeUser.vue
@@ -1,7 +1,25 @@
 <template>
   <v-container style="padding: 0">
     <v-card class="bridge-user">
-      <h3>Bridge asset Management on REI Network</h3>
+      <v-row justify="space-between">
+        <v-col cols="12" md="5"><h3>Bridge asset Management on REI Network</h3></v-col>
+        <v-col class="title-right" cols="12" md="6">
+          <v-row>
+            <v-btn text outlined color="validator">
+              <span class="iconfont">&#xe619;</span>
+            Submit Token Info
+          </v-btn>
+          <v-btn text outlined color="validator" @click="openGrantRole()">
+            <span class="iconfont">&#xe618;</span>
+            Add minter
+          </v-btn>
+          <v-btn text outlined color="validator" @click="openGreatToken()">
+            <span class="iconfont">&#xe601;</span>
+            Create your token
+          </v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
       <v-tabs v-model="tab" align-with-title hide-slider class="vote-list" background-color="background">
         <v-radio-group v-model="radios" mandatory row dense style="margin-top: 0" class="trend-tab">
           <v-tab key="1"> <v-radio label="Cbridge" value="1" class="trends-radio"> </v-radio></v-tab>
@@ -40,6 +58,9 @@
                         <span class="label-text">{{ item.target }}</span>
                     </div>
                   </template>
+                   <template v-slot:item.address="{ item }">
+                     <span class="minter-address">{{item.address | addr}}</span>
+                  </template>
                   <template v-slot:item.minter="{ item }">
                     <v-row justify="space-between" class="minter-cap">
                       <span>Used Amount: 360,510.69</span>
@@ -54,9 +75,14 @@
                       </div>
                     </div>
                   </template>
-                  <!-- <template v-slot:item="{ item }">
-     
-                  </template> -->
+                  <template v-slot:item.operation="{ item }">
+                  <v-btn  small color="vote_button" class="mr-2" style="color: #fff" @click="openMinterCup(item)" height="32">
+                      Set Minter Cap
+                    </v-btn>
+                    <v-btn  small color="start_unstake" class="mr-2" @click="openRevoke(item)" height="32">
+                      Revoke
+                    </v-btn>
+                  </template>
                 </v-data-table>
                 <div class="text-pagination pt-2" v-if="bridgeList.length > 0">
                   <v-pagination
@@ -110,7 +136,7 @@
                     <v-progress-linear color="#6979F8" rounded background-color="#F5F5F5" :value="item.minter"></v-progress-linear>
                     <div class="process">
                       <div>0</div>
-                      <div :style="{marginLeft:item.menuUp+'%'}">
+                      <div :style="{marginLeft:item.menuUp+'%'}" class="minter-item">
                         <v-icon color="#6979F8">mdi-menu-up</v-icon>
                         <span>{{item.minter}}%</span>
                       </div>
@@ -131,13 +157,236 @@
           </v-row>
         </v-tab-item>
       </v-tabs-items>
+      <v-dialog v-model="setMinterCupDialog" width="500">
+        <v-card class="minter-card">
+          <v-row justify="space-between" class="dialog-title">
+            <v-col cols="12" md="10">
+              <h3>Set Minter Cap</h3>
+            </v-col>
+           <v-col cols="12" md="1" @click="cancelMinterCup()" class="close-dialog">
+              <v-icon>mdi-close</v-icon>
+            </v-col>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Token</span>
+            <strong>Binance USD (BUSD) </strong>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Target Chain</span>
+            <strong>BNB Smart Chain</strong>
+          </v-row>
+          <v-row justify="space-between" class="set-minter">
+            <div>
+              <span class="left-title">Used Amount:</span> 
+              <strong> 360,510.69 </strong>
+            </div>
+            <div>
+              <span class="left-title">Max:</span> 
+              <strong> 10M </strong> 
+              <span class="left-title">USDT</span>
+            </div>
+          </v-row>
+          <v-progress-linear color="#6979F8" rounded background-color="#F5F5F5" value="70%"></v-progress-linear>
+          <div class="">
+            <div>0</div>
+            <div  class="minter-item" style="margin-left:70%">
+              <v-icon color="#6979F8">mdi-menu-up</v-icon>
+              <span>70%</span>
+            </div>
+          </div>
+          <div class="min-minter ">
+            <span class="left-title">Min Minter Cap:</span> 
+            <strong> 360,510,69 </strong>
+            <span class="left-title">USDT</span>
+          </div>
+          <v-row class="from-voting" justify="space-between">
+              <v-col  cols="12" md="2">
+                <div class="input-title">Minter Cap</div>
+              </v-col>
+                <v-col cols="12" md="10">
+                  <v-text-field 
+                    required
+                    outlined 
+                    background-color="input_other" 
+                    class="text-filed"
+                    :rules="addressRules"
+                  >
+                </v-text-field>
+                </v-col>
+            </v-row>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="RevokeDialog" width="500">
+        <v-card class="minter-card">
+          <v-row justify="space-between" class="dialog-title">
+            <v-col cols="12" md="10">
+              <h3>Do you want to revoke this Minter ?</h3>
+            </v-col>
+            <v-col cols="12" md="1" @click="cancelRevoke()" class="close-dialog">
+              <v-icon>mdi-close</v-icon>
+            </v-col>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Token</span>
+            <strong>Binance USD (BUSD) </strong>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Minter Cap</span>
+            <strong>10M</strong>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Target Chain</span>
+            <strong>BNB Smart Chain</strong>
+          </v-row>
+          <v-divider class="revoke-divider" />
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Minter Address</span>
+            <strong>0xADbBf74bc8d9CFfeC78526169cd81FdcBbA35eC2</strong>
+          </v-row>
+          <div class="text-center">
+             <v-btn  small outlined color="#868E9E" class="mr-4 revoke-btn" height="32" width="80">
+                no
+            </v-btn>
+            <v-btn  small color="vote_button" class="mr-4 revoke-btn" style="color: #fff" height="32" width="80">
+                yes  
+            </v-btn>
+          </div>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="grantRoleDialog" width="580">
+        <v-card class="minter-card">
+          <v-row justify="space-between" class="dialog-title" no-gutters>
+            <v-col cols="12" md="10">
+              <h3>Create Your Token</h3>
+            </v-col>
+            <v-col cols="12" md="1" @click="cancelGrantRole()" class="close-dialog">
+              <v-icon>mdi-close</v-icon>
+            </v-col>
+          </v-row>
+          <v-row no-gutters>
+            <v-col cols="12" md="3" class="left-title contract">Contract Address</v-col>
+            <v-col cols="12" md="9">
+              <v-combobox
+               outlined
+               label="Combobox"
+               :items="ContractItems"
+               >
+               </v-combobox>
+            </v-col>
+          </v-row>
+          <div>
+            <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Token</span>
+            <strong>Binance USD (BUSD) </strong>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Target Chain</span>
+            <strong>BNB Smart Chain</strong>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Decimals</span>
+            <strong>18 </strong>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Total Supply</span>
+            <strong>1,000,000.00</strong>
+          </v-row>
+          </div>
+          <v-row no-gutters>
+            <v-col cols="12" md="2" class="left-title contract">Minter Address</v-col>
+            <v-col cols="12" md="9">
+              <v-combobox
+               outlined
+               label="Combobox"
+               :items="items"
+               >
+               </v-combobox>
+            </v-col>
+            <v-col cols="12" md="1">
+             <v-img src="../assets/images/add.png"  class="add-plus" />
+            </v-col>
+          </v-row>
+          <div>
+             <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Token</span>
+            <strong>Binance USD (BUSD) </strong>
+          </v-row>
+          <v-row justify="space-between" class="title-row" no-gutters>
+            <span class="left-title">Target Chain</span>
+            <strong>BNB Smart Chain</strong>
+          </v-row>
+          </div>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="greatTokenDialog" width="500">
+        <v-card class="minter-card">
+          <v-row justify="space-between" class="dialog-title">
+            <v-col cols="12" md="10">
+              <h3>Create Your Token</h3>
+            </v-col>
+           <v-col cols="12" md="1" @click="cancelGreatToken()" class="close-dialog">
+              <v-icon>mdi-close</v-icon>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <div class="create-field">
+                <div class="left-title">Contract Address</div>
+                <v-text-field 
+                  label="1-16 characters" 
+                  required 
+                  outlined 
+                  background-color="input_other" 
+                  class="text-filed from-voting"
+                  :rules="addressRules"
+                  >
+                </v-text-field>
+              </div>
+              <div class="create-field">
+                <div class="left-title">Token Name</div>
+                <v-text-field 
+                  label="1-16 characters" 
+                  required 
+                  outlined 
+                  background-color="input_other" 
+                  class="text-filed from-voting"
+                  :rules="TokenRules"
+                  >
+                </v-text-field>
+              </div>
+              <div class="create-field">
+                <div class="left-title">Decimals</div>
+                <v-text-field 
+                  label="Default value : 18" 
+                  required 
+                  outlined 
+                  background-color="input_other" 
+                  class="text-filed from-voting"
+                  :rules="addressRules"
+                  >
+                </v-text-field>
+              </div>
+            </v-col>
+          </v-row>
+          <v-row justify="space-between">
+            <div>
+              <span class="left-title">Services Fees : </span>
+              <strong class="service-fees">10 </strong>
+              <span class="left-title">REI</span>
+            </div>
+            <v-btn  small color="vote_button" class="mr-4" style="color: #fff" height="32" width="120">
+                Grant 
+            </v-btn>
+          </v-row>
+        </v-card>
+      </v-dialog>
     </v-card>
   </v-container>
 </template>
 <script>
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-
+import util from '../utils/util';
 export default {
   data() {
     return {
@@ -148,23 +397,35 @@ export default {
       itemsPerPage: 10,
       menuUp:0,
       stakeListLoading: false,
+      setMinterCupDialog:false,
+      RevokeDialog:false,
+      grantRoleDialog:false,
+      greatTokenDialog:false,
       headers: [
             {text:'Label', value: 'label'},
             { text: 'Target Chain', value: 'target' },
             { text: 'Minter Cap', value: 'minter' },
+            { text: 'Address', value: 'address' },
+            { text: 'Operation', value: 'operation' },
         ],
         bridgeList:[
           {
             label:"USDT",
             target:"Binance Smart Chain",
+            address:"gdt61...83265",
             minter:60,
           },
           {
             label:"USDT",
             target:"Binance Smart Chain",
+            address:"gd003...r3216",
             minter:88,
           }
-        ]
+        ],
+        ContractItems:["1k3o348brr40kk092933","00000yu8760k67234333"],
+        items:["1q2w3e4r5t6y7u8iou","0a9s8d7f6gh5j4k3l"],
+        addressRules: [(v) => !!v || this.$t('msg.please_input_address')],
+        TokenRules:[(v) => !!v || "Please enter the token"]
     };
   },
   watch: {},
@@ -181,6 +442,32 @@ export default {
           menuUp:menuUp
         }
       })
+    },
+    openMinterCup(value){
+      this.setMinterCupDialog = true;
+      console.log('value',value)
+    },
+    cancelMinterCup(){
+      this.setMinterCupDialog = false;
+    },
+    openRevoke(value){
+      this.RevokeDialog = true;
+      console.log('value2',value)
+    },
+    cancelRevoke(){
+      this.RevokeDialog = false;
+    },
+     openGrantRole(){
+      this.grantRoleDialog = true;
+    },
+    cancelGrantRole(){
+      this.grantRoleDialog = false;
+    },
+    openGreatToken(){
+      this.greatTokenDialog = true;
+    },
+    cancelGreatToken(){
+      this.greatTokenDialog = false;
     }
   }
 };
@@ -190,12 +477,21 @@ export default {
 .vote-list{
   margin-top:30px;
 }
+.v-btn {
+  text-transform: none !important;
+  }
 .bridge-user {
   padding: 20px;
   .v-tab {
     text-transform: none !important;
     margin-left: 0 !important;
   }
+.title-right{
+  margin-top: 10px;
+  .v-btn.v-btn--outlined.v-btn--text{
+    border:none;
+  }
+}
   .theme--dark.v-application .text-pagination[data-v-b6724c44][data-v-b6724c44]{
     background-color:#1d1a36;
   }
@@ -208,16 +504,70 @@ export default {
     }
     .label-text{
       margin-left: 13px;
-      margin-right: 38px;
+      margin-right: 12px;
+    }
+    .minter-address{
+      font-size: 12px;
+      color: lightgray;
+      
     }
   }
-  .minter-cap{
+}
+ .minter-cap{
     padding: 12px;
     font-size: 12px;
+    width: 340px;
+  }
+  .set-minter{
+    padding: 12px;
   }
   .process{
     font-size: 12px;
     display: flex;
+  }
+  .left-title{
+    color: #868E9E;
+  }
+.minter-card{
+  padding: 30px;
+  .dialog-title{
+    margin-bottom: 20px;
+  }
+  .title-row{
+    margin: 8px 0;
+  }
+  .revoke-divider{
+    margin: 16px 0;
+  }
+  .min-minter{
+    text-align: right; 
+  }
+  .create-field{
+    margin-top:8px;
+  }
+  .from-voting {
+  margin-top: 12px;
+  .input-title {
+    color: #868e9e;
+    margin-top: 15px;
+    text-align: center;
+    height: 40px;
+    }
+  }
+.text-center{
+  margin-top:20px;
+  text-align: right !important;
+  }
+  .service-fees{
+    font-size: 28px;
+  }
+  .contract{
+    margin-top: 12px;
+  }
+  .add-plus{
+   margin-top:8px;
+  text-align: right;
+  display: inline-block;
   }
 }
 @media screen and (max-width: 900px) {
