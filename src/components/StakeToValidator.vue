578d742608b840a037c64ce72db70a46a2324b6e
haifun <haifuncn@gmail.com>
2023-03-01 18:07:11 +0800

    chore: modify jail list

--- src/components/StakeToValidator.vue
+++ src/components/StakeToValidator.vue
@@ -143,7 +143,7 @@
                 <div>{{ (item.timestamp * 1000) | dateFormat('YYYY-MM-dd hh:ss:mm') }}</div>
               </template>
               <template v-slot:item.operation="{ item }">
-                <div v-if="item.address.toUpperCase() !== connection.address.toUpperCase()">
+                <div v-if="item.address.toUpperCase() == connection.address.toUpperCase()">
                   <v-btn tile small color="vote_button" class="mr-4 font-btn btn-radius" @click.stop="getPayFine(item)" height="32"> Pay Fine </v-btn>
                 </div>
                 <div v-else>-</div>
